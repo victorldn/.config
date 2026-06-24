ab3f60c7997c2578272f94d5811bcc1f7eed3b35
Victor Do Nascimento <victor.donascimento@arm.com>
2026-06-24 17:34:17 +0100

    force fix todo list 2 (is it needed?)

--- eww/start.sh
+++ eww/start.sh
@@ -2,14 +2,22 @@
 
 eww daemon
 
-sleep 1
+for _ in $(seq 1 30); do
+  eww state >/dev/null 2>&1 && break
+  sleep 1
+done
 
-eval "$(
-  ~/.config/eww/scripts/org-default-task.sh
-)"
+for _ in $(seq 1 60); do
+  emacsclient --eval '(progn (require '\''org) (emacs-pid))' >/dev/null 2>&1 && break
+  sleep 1
+done
+
+eval "$("$HOME/.config/eww/scripts/org-default-task.sh")"
 
 eww update \
   active_task_id="$active_task_id" \
   active_task_desc="$active_task_desc"
 
-eww update quick_notes_yuck="$(~/.config/eww/scripts/org-task-notes-yuck.sh)"
+eww update tasks_yuck="$("$HOME/.config/eww/scripts/org-table.yuck.sh")"
+eww update quick_notes_yuck="$("$HOME/.config/eww/scripts/org-task-notes-yuck.sh")"
+eww update active_task_time="$("$HOME/.config/eww/scripts/org-task-clock-total.sh")"
